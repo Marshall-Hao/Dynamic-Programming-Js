3db9d524dbecf6a71d9eb98ec7a93304dd5fd422
marshall chen <marshallchan666@outlook.com>
2021-07-07 21:33:49 +0800

    count-construct tab done

--- tabulation/can-construct.js
+++ tabulation/can-construct.js
@@ -2,7 +2,7 @@ const canConstruct = (target, wordBank) => {
     const table = Array(target.length + 1).fill(false);
     table[0] = true;
 
-    for (let i = 0; i <= target; i++) {
+    for (let i = 0; i <= target.length; i++) {
         if (table[i] === true) {
             for (let word of wordBank) {
                 if (target.slice(i, i + word.length) === word) {
